694cd6fe0983a24b2ff6ab47e4bf90a3f9941198
nqhbao2023 <144966377+nqhbao2023@users.noreply.github.com>
2025-12-12 11:24:35 +0700

    update

---
+++ server/src/types/express/index.d.ts
@@ -0,0 +1,12 @@
+import "express";
+
+declare global {
+  namespace Express {
+    interface Request {
+      file?: Multer.File;
+      files?: Multer.File[] | { [fieldname: string]: Multer.File[] };
+    }
+  }
+}
+
+export {};
